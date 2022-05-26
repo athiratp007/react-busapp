e5c82fa27caf4a49b92571a7a8def751cff75ec2
athiratp007 <athiratp007@example.com>
2022-05-27 04:07:42 +0530

    :nn

---
+++ src/Components/Addbus.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const>   
+
+= () => {
+  return (
+    <div>   
+
+        
+    </div>
+  )
+}
+<div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Route</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Bus name</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Bus regno</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for=".form-label">Owner name</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Contact no</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button class="btn btn-success"SUBMIT></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+export default>   
+
